1ad0510a8e3d5ad0697685a79ce2075366e54347
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-03-11 20:49:24 +0000

    change debug

--- main/settings.py
+++ main/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 if os.path.exists('env.py'):
     import env
-    
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +25,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [ '8000-timothyyw-gamesreviewpr-ocw723hr2uc.ws-eu108.gitpod.io']
 
