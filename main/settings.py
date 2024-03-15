d99669f7adcbd9e6a55783c5f3883ef177b95eed
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-03-15 14:25:23 +0000

    fix block issue and test

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['8000-timothyyw-gamesreviewpr-ocw723hr2uc.ws-eu109.gitpod.io']
+ALLOWED_HOSTS = ['8000-timothyyw-gamesreviewpr-ocw723hr2uc.ws-eu110.gitpod.io']
 
 
 # Application definition
