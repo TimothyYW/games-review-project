4bc85acb8ed6e41ea219bf120074636a2c3eadf9
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-03-20 21:59:50 +0000

    finish setting up models

--- reviews/models.py
+++ reviews/models.py
@@ -1,19 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from djrichtextfield.models import RichtextField
+from djrichtextfield.models import RichTextField
 from django_resized import ResizedImageField
+
+# Games type
+GAME_TYPE = (
+    ('multiplayer', 'Multiplayer'),
+    ('single_player', 'Single Plyer')
+)
+
+GENRE_TYPE = (
+    ('horror', 'Horror'),
+    ('action', 'Action'),
+    ('story_telling', 'Story telling'),
+)
 
 class Review(models.Model):
     """
     A model to add and manage reviews
     """
-    user = models.ForgenKey(User, related_name='review_owner', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='review_owner', on_delete=models.CASCADE)
     game_name = models.CharField(max_length=300, null=False, blank=False)
-    review = RichtextField(max_length=500, null=False, blank=False)
+    review = RichTextField(max_length=500, null=False, blank=False)
     image = ResizedImageField(
         size = [400, None], quality=80, upload_to='game/', force_format='WEBP',
         blank=False, null=False
     )
-image_alt = models.CharField(max_length=100, null=False, blank=False)
-type_game = modelsCharField(max_length=50, null=False, blank=False)
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    type_game = models.CharField(max_length=50, choices=GAME_TYPE, default="Single Player")
+    genre = models.CharField(max_length = 50, choices=GENRE_TYPE, default="Games")
+    developer = models.CharField(max_length=20, null=False, blank=False)
+    posted = models.DateTimeField(auto_now=True)
+
+class Meta:
+    ordering = ['posted_date']
+
+def __str__(self):
+    return str(self.title)
