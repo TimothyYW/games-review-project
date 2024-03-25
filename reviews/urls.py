c30aba677fb5a9628217aee523a599493cbca2dc
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-03-25 23:09:43 +0000

    create reviews input function

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import InputReview
+from .views import InputReview, Reviews
 
 
 urlpatterns = [
     path('', InputReview.as_view(), name='input_review'),
-    
+    path('reviews/',Reviews.as_view(), name='reviews'), 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,18 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Review
+from .forms import Reviewform
+
+
+class Reviews(ListView):
+    """ View all reviews """
+    template_name = "reviews/reviews.html"
+    model = Review
+    context_object_name = 'reviews'
+
+
 class InputReview(LoginRequiredMixin, CreateView):
     """ Add review view """
     template_name = 'reviews/input_review.html'
