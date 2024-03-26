9b60392f68c847baed3d540785ffaee52313b67e
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-03-26 20:41:16 +0000

    create delete function

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView
+
+from django.contrib.auth.mixins import (
+    UserPassesTextMixin, LoginRequiredMixin
+)
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -22,3 +26,11 @@ class InputReview(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(InputReview, self).form_valid(form)
+
+class DeleteReview(LoginRequiredMixin, UserPassesTextMixin, DeleteView):
+    """ Delete a review """
+    model = Review
+    success_url = '/reviews/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
