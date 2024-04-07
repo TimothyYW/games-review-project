9bdc21e7256581a56bd5d8cbbd7e27988a45704d
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-04-07 12:00:55 +0000

    create edit profile function

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'user',
-        'bio',
-        'image'
-    )
 
-admin.site.register(Profile, ProfileAdmin) 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "bio", "image")
+
+
+admin.site.register(Profile, ProfileAdmin)
 # Register your models here.
--- profiles/views.py
+++ profiles/views.py
@@ -4,14 +4,15 @@ from .models import Profile
 
 
 class Profiles(TemplateView):
-    """ User Profile View"""
+    """User Profile View"""
+
     template_name = "profiles/profile.html"
 
     def get_context_data(self, **kwargs):
         profile = Profile.objects.get(user=self.kwargs["pk"])
-        context = {
-            'profile' : profile
-        }
+        context = {"profile": profile}
 
         return context
+
+
 # Create your views here.
