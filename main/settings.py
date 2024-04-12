b67758ea25a490a0d5bc1a685fbfa2b85c6661dc
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-04-12 19:11:09 +0000

    WIP

--- main/settings.py
+++ main/settings.py
@@ -173,6 +173,8 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
 
 # Cloudinary settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
