b067049ec84ff8042c4d2039b5f440db252c844d
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-04-12 19:36:19 +0000

    WIP

--- main/settings.py
+++ main/settings.py
@@ -174,7 +174,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-​STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# ​STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_DIR = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
