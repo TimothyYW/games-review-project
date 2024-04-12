fc31cad8b7022c5e75b526b76cc249433e7ab8b9
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2024-04-12 20:30:59 +0000

    update for heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -83,7 +83,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
 
 CRISPY_ALLOWED_TEMPLATES_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
@@ -173,10 +175,11 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 
